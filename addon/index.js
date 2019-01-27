aa25d1b1e6bb89a511a676efa4df2e9f0164b201
Paul Chavard <git@paul.chavard.net>
2019-01-27 18:02:42 +0100

    Experimental support for refetch and fetchMore

--- addon/index.js
+++ addon/index.js
@@ -9,9 +9,30 @@ export function getObservable(result) {
   return meta && meta.observable;
 }
 
+function callObservable(result, method, opts) {
+  const meta = getMeta(result);
+  if (meta && meta.observable) {
+    const { observable, service } = meta;
+
+    return service.waitFor((resolve, reject) => {
+      observable[method](opts)
+        .then(resolve)
+        .catch(reject);
+    });
+  }
+}
+
 export function unsubscribe(result) {
   const meta = getMeta(result);
   if (meta && meta.unsubscribe) {
     meta.unsubscribe();
   }
+}
+
+export function refetch(result, opts) {
+  return callObservable(result, 'refetch', opts);
+}
+
+export function fetchMore(result, opts) {
+  return callObservable(result, 'fetchMore', opts);
 }
